ada5de0c83d5a85b81652db024b6efad143b34e0
leiqichn <lei_qi@outlook.com>
2023-03-14 21:38:13 +0800

    gitee update

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,4 @@
 git add .
 git commit -m "update"
 git push origin master
-git push github master
-
-sleep 60
-git checkout gh-pages
-git add . 
-
-git commit -m "update"
-git push origin master
-
-git checkout master
+git push gitee master
---
+++ sync2gitee.sh
@@ -0,0 +1,9 @@
+
+git checkout gh-pages
+git pull origin gh-pages
+git add . 
+
+git commit -m "gitee update"
+git push gitee gh-pages
+
+git checkout master
