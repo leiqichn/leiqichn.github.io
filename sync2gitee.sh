479388ac996a594f923c40e84cfaf7d94fc8bc37
leiqichn <lei_qi@outlook.com>
2023-03-14 21:41:59 +0800

    gitee update master

--- sync2gitee.sh
+++ sync2gitee.sh
@@ -1,9 +1,14 @@
 
+git add . 
+git commit -m "gitee update master"
+echo "###push gitee master###"
+git push gitee master
 git checkout gh-pages
+echo "###pull gitee gh-pages###"
 git pull origin gh-pages
 git add . 
-
-git commit -m "gitee update"
+git commit -m "gitee update gh-pages"
+echo "###push gitee gh-pages###"
 git push gitee gh-pages
 
 git checkout master
