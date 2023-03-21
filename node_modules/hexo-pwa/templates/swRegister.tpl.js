be59e26bbe03fa33be11dbf089566747361da3bb
leiqichn <lei_qi@outlook.com>
2023-03-21 23:58:08 +0800

    update

--- node_modules/hexo-pwa/templates/swRegister.tpl.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('<%=path %>')
-    .then(function () {console.log('ServiceWorker Register Successfully.')})
-    .catch(function (e) {console.error(e)});
-}
