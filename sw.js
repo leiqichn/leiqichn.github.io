2f86adfbf65b0fe305b068ac484cd8748f54ede5
leiqichn <leiqichn@users.noreply.github.com>
2024-06-24 15:13:15 +0000

    deploy: 694720d6173dd7e4eabfa1b098f5420864a7dbfe

--- sw.js
+++ sw.js
@@ -27,15 +27,15 @@ var precacheUrls = [
 
   '/',
 
+  '/2024-06-24-6a7053746b1e.html',
+
+  '/2024-06-24-1130f46a1ad4.html',
+
   '/2024-06-21-02cd02e81f22.html',
 
   '/2024-05-20-1fbf9ee2486f.html',
 
   '/2024-05-18-dfe0612e31db.html',
-
-  '/2024-05-10-b1491b9671d6.html',
-
-  '/2024-05-08-9a58f8b87168.html',
 
 ];
 toolbox.precache(precacheUrls);
