d506191066a28ef4471e62e7bb9b027372174277
leiqichn <lei_qi@outlook.com>
2026-06-27 08:02:18 +0800

    Site updated: 2026-06-27 08:02:15

--- sw.js
+++ sw.js
@@ -27,6 +27,8 @@ var precacheUrls = [
 
   '/',
 
+  '/2026-06-27-20ddbe1d7ec8.html',
+
   '/2026-06-26-06e2b0730d8a.html',
 
   '/2026-06-25-a6cb17b9a059.html',
@@ -34,8 +36,6 @@ var precacheUrls = [
   '/2026-06-24-80df7ffc0f48.html',
 
   '/2026-06-23-21ba9a1a5352.html',
-
-  '/2026-06-22-8d2c04f9ba86.html',
 
 ];
 toolbox.precache(precacheUrls);
