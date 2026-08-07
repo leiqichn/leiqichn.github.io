bad7256a2873314ad09b73e6515fe21a8e5389f1
leiqichn <lei_qi@outlook.com>
2026-08-07 08:01:49 +0800

    Site updated: 2026-08-07 08:01:45

--- sw.js
+++ sw.js
@@ -27,6 +27,8 @@ var precacheUrls = [
 
   '/',
 
+  '/2026-08-07-3344243c4f0d.html',
+
   '/2026-08-06-68a06c7aa2ef.html',
 
   '/2026-08-05-21ec8807dd30.html',
@@ -34,8 +36,6 @@ var precacheUrls = [
   '/2026-08-04-05f71e680e41.html',
 
   '/2026-08-03-2481725529f0.html',
-
-  '/2026-08-02-493956a89e74.html',
 
 ];
 toolbox.precache(precacheUrls);
