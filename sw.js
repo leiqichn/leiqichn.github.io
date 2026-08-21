8b4544a5325d4dfcff3c1c20109d979d9c27af8f
leiqichn <lei_qi@outlook.com>
2026-08-21 08:02:39 +0800

    Site updated: 2026-08-21 08:02:36

--- sw.js
+++ sw.js
@@ -27,6 +27,8 @@ var precacheUrls = [
 
   '/',
 
+  '/2026-08-21-2b6c8dc61481.html',
+
   '/2026-08-20-0a2cae189804.html',
 
   '/2026-08-19-38b47597047d.html',
@@ -34,8 +36,6 @@ var precacheUrls = [
   '/2026-08-18-1eb75184c011.html',
 
   '/2026-08-17-33c371408a11.html',
-
-  '/2026-08-16-4cba87551ec2.html',
 
 ];
 toolbox.precache(precacheUrls);
